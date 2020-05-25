ddcbdbffa68b241dbcb622961e57dbe5d5055c31
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2020-05-25 21:07:07 +0200

    install, clean and write clean data

---
+++ 00-install-prerequisites.R
@@ -0,0 +1,18 @@
+# Install required packages
+
+# install packages for data prep
+install.packages("devtools")
+install.packages("openxlsx")
+install.packages("lubridate")
+install.packages("shiny")
+
+# install packages for modelling
+install.packages("ranger")
+install.packages("gbm")
+install.packages("xgboost")
+install.packages("rms")
+install.packages("mlr")
+
+# install packages for XAI
+install.packages("DALEX")
+
