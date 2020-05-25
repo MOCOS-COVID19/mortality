e9be8237c959b88f2b8730be1ec6f452cb6e0278
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2020-05-25 23:35:05 +0200

    + length of hospitalisation

--- 02-app.R
+++ 02-app.R
@@ -9,6 +9,9 @@ library(mlr)
 # some additional cleaning
 small_data <- readRDS("small_data.rds")
 colnames(small_data)[8] = "Zgon"
+colnames(small_data)[9] = "Hospitalisation"
+small_data$Hospitalisation[small_data$Hospitalisation == -100] = NA
+
 colnames(small_data)[7] = "Przebieg"
 levels(small_data$Przebieg)[1] = "Brak"
 small_data$Zgon <- ifelse(small_data$Zgon, "Tak", "Nie")
@@ -25,7 +28,8 @@ ui <- fluidPage(
     fluidRow(
         column(4, 
                selectInput("model", "Rodzaj modelu:", c("xgboost", "ranger", "gbm", "rms"), "ranger"),
-               selectInput("target", "Zmienna celu:", c("Zgon", "Gorączka", "Kaszel", "Duszność"), "ranger"),
+               selectInput("target", "Zmienna celu:", c("Zgon", "Gorączka", "Kaszel", "Duszność", "Hospitalisation"), "Zgon"),
+               checkboxInput("monotonicity", "Monotonicity", TRUE),
                selectInput("dependent", "Zmienne objaśniające:", 
                            c("Wiek", "Płeć", "Środowisko", "Gorączka", "Kaszel", "Duszność", 
                              "Przebieg", "Zgon"), 
@@ -42,23 +46,30 @@ ui <- fluidPage(
 server <- function(input, output) {
     
     train_model <- reactive({
+        if (input$target == "Hospitalisation") {
+            small_data <- na.omit(small_data)
+            y <- small_data[,input$target] 
+        } else {
+            y <- small_data[,input$target] == "Tak"
+        }
         x <- small_data[,input$dependent]
-        y <- small_data[,input$target] == "Tak"
         formul <- paste("y ~ ", paste(input$dependent, collapse = " + "))
         model_ex <- switch (input$model,
                ranger = {
-                   model <- ranger(y = y, x = x, probability = TRUE, classification = TRUE)   
+                   model <- ranger(y = y, x = x, 
+                                   probability = (input$target != "Hospitalisation"), 
+                                   classification = (input$target != "Hospitalisation"))   
                    explain(model, data = x, y = y)
                },
                gbm = {
                    model <- gbm(as.formula(formul), data = data.frame(y = y, x),
-                                var.monotone = as.numeric(colnames(x) == "Wiek"))   
+                                var.monotone = as.numeric(colnames(x) == "Wiek") * input$monotonicity)   
                    explain(model, data = x, y = y)
                },
                xgboost = {
                    dat <- as.matrix(createDummyFeatures(x))
                    model <- xgboost(dat,  y, nrounds = 10,
-                                    monotone_constraints = as.numeric(colnames(dat) == "Wiek"))
+                                    monotone_constraints = as.numeric(colnames(dat) == "Wiek") * input$monotonicity)
                    explain(model, data = x, y = y,
                            predict_function = function(m,x){
                                x = as.matrix(createDummyFeatures(x))
@@ -67,7 +78,11 @@ server <- function(input, output) {
                },
                rms = {
                    formul <- gsub(formul, pattern = "Wiek", replacement = "rcs(Wiek)")
-                   model <- lrm(as.formula(formul), data = data.frame(y = y, x))   
+                   if (input$target == "Hospitalisation") {
+                       model <- ols(as.formula(formul), data = data.frame(y = y, x))   
+                   } else {
+                       model <- lrm(as.formula(formul), data = data.frame(y = y, x))   
+                   }
                    explain(model, data = x, y = y)
                }
         )
